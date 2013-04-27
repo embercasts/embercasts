0d4125aac5ffa1fbab57060c0a1a2068dff0e82c
Erik Bryn <erik.bryn@gmail.com>
2013-04-27 02:39:50 -0400

    Rough in HTML structure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,11 @@
 //= require ember
 //= require app
 //= require_tree .
+
+App.ApplicationRoute = Ember.Route.extend({
+  model: function() {
+    return [
+      {id: 1, title: "ZOMG", subtitle: "Herp derp", videoURL: "http://www.youtube.com/embed/Sc_O7ZWVSJ0?rel=0"}
+    ];
+  }
+});
