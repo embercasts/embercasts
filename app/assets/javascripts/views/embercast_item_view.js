3fa2f422e0fd6e60e3f57feed0a539ba19e4d8d5
Erik Bryn <erik.bryn@gmail.com>
2013-05-05 15:58:09 -0700

    Fix video loading issue after navigating

--- app/assets/javascripts/views/embercast_item_view.js
+++ app/assets/javascripts/views/embercast_item_view.js
@@ -1,5 +1,8 @@
 App.EmbercastItemView = Ember.View.extend({
   didInsertElement: function() {
     sublime.load();
+    if (sublime.prepare) { // wtf sublime video?!
+      sublime.prepare(this.get('context.youtube_id'));
+    }
   }
 });
