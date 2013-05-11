678f480478f004a17ab1f7da67f151bea0e68104
Dave Laird <dave@urbanspoon.com>
2013-05-10 20:18:32 -0700

    added footer nav

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,5 +1,8 @@
 App.Router.map(function() {
   this.resource('embercasts', function() {
     this.route('show', {path:'/:embercast_id'});
+    this.route('about');
+    this.route('contact');
   });
+
 });
