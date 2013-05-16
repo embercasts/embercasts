5f02f8311c9f7a5ad5bf9ea70e12d9f60a3b98ca
Erik Bryn <erik.bryn@gmail.com>
2013-05-16 12:55:10 -0700

    Remove Sublime Video

--- app/assets/javascripts/views/episode_view.js
+++ app/assets/javascripts/views/episode_view.js
@@ -1,15 +1,4 @@
 App.EpisodeView = Ember.View.extend({
   tagName: 'article',
-  templateName: 'episode',
-  didInsertElement: function() {
-    sublime.load();
-    if (sublime.prepare) { // wtf sublime video?!
-      sublime.prepare(this.get('context.youtube_id'));
-    }
-  },
-
-  share: function() {
-    console.log(this.get('showLinks'));
-    return this.toggleProperty('showLinks');
-  }
+  templateName: 'episode'
 });
