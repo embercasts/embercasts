7053e1f7b01cb91c3dc97d27c0ad16db28ea4e8b
Erik Bryn <erik.bryn@gmail.com>
2013-05-17 02:09:12 -0700

    Trash unused code

--- app/assets/javascripts/routes/application_route.js
+++ app/assets/javascripts/routes/application_route.js
@@ -1,12 +1,5 @@
 App.ApplicationRoute = Ember.Route.extend({
   model: function() {
     return App.Embercast.find();
-  },
-
-  events: {
-    sharePopup: function(target, url) {
-      window.open(url, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=600,height=' + App.ShareLink.popupHeight(target));
-      return false;
-    }
   }
 });
